510f7dd8476d750cce4fcb9dec57532921eee4fc
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-06 13:45:35 -0300

    working on

--- scraper-opt.py
+++ scraper-opt.py
@@ -3,3 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
+
+# Function to scroll to bottom
+def scroll_to_bottom(driver, max_clicks=3):
+    for _ in range(max_clicks):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
