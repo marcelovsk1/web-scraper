30bb91ac5cf54108c4fe6ff13e2983ca8a74507d
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-06 14:06:49 -0300

    working on

--- filter.py
+++ filter.py
@@ -1,6 +1,6 @@
 import json
 
-def filter_events(events_data, event, date, location, image_url):
+def filter_events(events_data, event_name, date, location, image_url):
     filtered_events = []
     seen_events = set()
 
@@ -12,12 +12,12 @@ def filter_events(events_data, event, date, location, image_url):
         events = source_data['events']
 
         for event in events:
-            event_key = (event[event], event[date], event[location], event[image_url])
+            event_key = (event[event_name], event[date], event[location], event[image_url])
             if event_key not in seen_events:
                 seen_events.add(event_key)
                 filtered_events.append({
                     'source_name': source_name,
-                    'event_name': event[event],
+                    'event_name': event[event_name],
                     'date': event[date],
                     'location': event[location],
                     'image_url': event[image_url]
@@ -29,3 +29,5 @@ def filter_events(events_data, event, date, location, image_url):
 filtered_events = filter_events("events_data.json", "Event", "Date", "Location", "Image URL")
 for event in filtered_events:
     print(event)
+
+# yayy its working!
