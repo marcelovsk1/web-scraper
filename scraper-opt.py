ac4d6ceb6ae967c6c10faba0d83020d1c3ce6fa1
Marcelo Amaral Alves <marceloaa@live.com>
2024-02-06 13:46:33 -0300

    working on

--- scraper-opt.py
+++ scraper-opt.py
@@ -9,3 +9,14 @@ def scroll_to_bottom(driver, max_clicks=3):
     for _ in range(max_clicks):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
+
+def scrape_events(driver, url, selectors):
+    driver.get(url)
+    driver.implicitly_wait(30)
+    scroll_to_bottom(driver)
+
+    page_content = driver.page_source
+    webpage = BeautifulSoup(page_content, 'html.parser')
+
+    events = webpage.find_all(selectors['event']['tag'], class_=selectors['event'].get('class'))
+    event_list = []
